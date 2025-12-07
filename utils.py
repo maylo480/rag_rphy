5f891b38d006bb672b4199d6883fb7b0d821d0a9
Miłosz <mfgrysd99@gmail.com>
2025-12-07 12:30:58 +0100

    Init commit

---
+++ utils.py
@@ -0,0 +1,63 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from pgvector.sqlalchemy import Vector
+from sqlalchemy.orm import sessionmaker, declarative_base
+from pgvector.sqlalchemy import Vector
+
+Base = declarative_base()
+
+def get_surrounding_sentences(entry_ids, file_names, group_window_size, session):
+
+    surrounding_sentences = []
+    for entry_id, file_name in zip(entry_ids, file_names):
+        surrounding_sentences.append(
+            session.query(TextEmbedding.id, TextEmbedding.sentence_number, TextEmbedding.content, TextEmbedding.file_name)\
+            .filter(TextEmbedding.id >= entry_id - group_window_size)\
+            .filter(TextEmbedding.id <= entry_id + group_window_size)\
+            .filter(TextEmbedding.file_name == file_name).all()
+        )
+    
+    return surrounding_sentences
+
+# Finding the content from our database which is most similar to the query
+def search_embeddings(query_embedding, session, limit=5):
+    return session.query(TextEmbedding.id, TextEmbedding.sentence_number, TextEmbedding.content, TextEmbedding.file_name, 
+        TextEmbedding.embedding.cosine_distance(query_embedding).label("distance") )\
+        .order_by("distance").limit(limit).all()
+
+# Check if a matches context window overlaps with another matches context window.
+def is_unique_to_window(existing_matches, current_match, group_window_size=5):
+    
+    for match in existing_matches:
+        if match[3] != current_match[3]:
+            continue
+        if match[1] > current_match[1] + group_window_size or match[1] < current_match[1] - group_window_size:
+            continue
+        else:
+            return False
+    
+    return True
+
+# Getting unique matches from search results
+def get_filtered_matches(search_results):
+    unique_count = 0
+    matches = []
+
+    for result in search_results:
+        if unique_count >= 5:
+            break;
+        if is_unique_to_window(matches, result):
+            unique_count += 1
+        matches.append(result)
+
+    return matches
+
+class TextEmbedding(Base):
+    __tablename__ = 'text_embeddings'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    embedding = Column(Vector)
+    content = Column(String)
+    file_name = Column(String)
+    sentence_number = Column(Integer)
+
+    def __str__(self):
+        return self.content + " " + str(self.id)
